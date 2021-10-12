7d92f973b34d78e65322309c4c14d21d7e7cf080
Gunnar Beushausen <info@gunnar-beushausen.de>
2021-10-12 22:04:29 +0200

    input validation

--- cdk/lib/pipeline-stack.ts
+++ cdk/lib/pipeline-stack.ts
@@ -1,13 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+import {RemovalPolicy} from '@aws-cdk/core';
 import * as codecommit from '@aws-cdk/aws-codecommit';
 
 export class PipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // Creates a CodeCommit repository called 'WorkshopRepo'
-    new codecommit.Repository(this, 'ILuvCoffee', {
-      repositoryName: 'ILuvCoffee',
-    });
-  }
+        // Creates a CodeCommit repository called 'WorkshopRepo'
+        const repo = new codecommit.Repository(this, 'ILuvCoffee', {
+            repositoryName: 'ILuvCoffee',
+        });
+
+        repo.applyRemovalPolicy(RemovalPolicy.RETAIN);
+    }
 }
